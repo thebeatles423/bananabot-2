a833a70f57637299b5ff9af57a79416bc73c3822
ksbidlack <ksbidlack@gmail.com>
2022-12-04 17:58:32 -0800

    fixed some more small bugs

--- src/main.py
+++ src/main.py
@@ -16,6 +16,23 @@ intents = discord.Intents.all()
 bot = commands.Bot(command_prefix="b.", intents=intents)
 
 
+# when the bot starts up
+@bot.event
+async def on_ready():
+    print("Bot is connected to discord!")
+
+
+# error handling
+@bot.event
+async def on_command_error(ctx, error):
+    ignored_errors = (commands.CommandNotFound, commands.UserInputError)
+    if isinstance(error, ignored_errors):
+        return
+    elif isinstance(error, commands.CheckFailure):
+        await ctx.reply("You don't have the permissions to use this command!")
+        return error
+
+
 # run code -- see run.py
 @bot.command(name="run")
 @commands.is_owner()
@@ -23,10 +40,10 @@ async def run_code(ctx, *, code_input: str):
     try:
         language = code_input.splitlines()[0].split("```")[1]
     except:
-        return await ctx.reply(f"`An unknown error occurred.\nDid you format the code correctly?`")
+        return await ctx.reply("An unknown error occurred. Did you format the code correctly?")
     
     if language not in run.supported_languages:
-        return await ctx.reply("`Unsupported language!`")
+        return await ctx.reply("Unsupported language! Did you format the code correctly?")
     else:
         await run.run_code(ctx, language, code_input, global_vars={"ctx": ctx})
 
--- src/run.py
+++ src/run.py
@@ -10,13 +10,27 @@ async def run_code(ctx, language, code_input, global_vars: dict):
         # change stdout
         with io.StringIO() as output, redirect_stdout(output):
             try:
-                # assumes the ending backticks '```' are on a new line
-                code = compile('\n'.join(code_input.splitlines()[1:-1]), "temp", "exec")
+                if code_input.splitlines()[-1] == "```": # if the ending backticks '```' are on a new line
+                    code = compile('\n'.join(code_input.splitlines()[1:-1]), "temp", "exec")
+                elif code_input[-3:] == "```":
+                    code_input = code_input[:-3]
+                    code = compile('\n'.join(code_input.splitlines()[1:]), "temp", "exec")
+                else:
+                    return await ctx.reply("An unknown error occurred. Did you format the code correctly?")
                 # execute the compiled code
                 exec(code, global_vars)
             except:
                 # send error to stdout
-                print(f"```{traceback.format_exc()}```") 
-            
-            output = output.getvalue()
-        return await ctx.reply(f"```{output}```")
+                print(traceback.format_exc())
+            finally:
+                output = output.getvalue()
+        
+        # if the output has a ` character, it needs to be escaped
+        for char, char_index in enumerate(output):
+            if char == '`':
+                output = output[:char_index] + '\\' + output[char_index:]
+        
+        if output:
+            return await ctx.reply(f"```{output}```")
+        else: # if there is no output to stdout
+            return await ctx.reply("Your code returned nothing to stdout.")
