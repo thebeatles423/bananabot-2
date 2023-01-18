51bdb51df539f688204eeb06050f83a697ed3857
ksbidlack <ksbidlack@gmail.com>
2023-01-17 17:24:56 -0800

    add message source language to translate command

--- src/ext/translate.py
+++ src/ext/translate.py
@@ -1,17 +1,16 @@
 import discord
 from discord.ext import commands
 from googletrans import Translator
+from googletrans import LANGUAGES
 
 translator = Translator()
 
 
 class TranslateView(discord.ui.View):
-    def __init__(self, msglink):
+    def __init__(self, msglink, src_lang: str):
         super().__init__()
 
-        self.msglink = msglink
-
-        link_button = discord.ui.Button(label="Message Link", style=discord.ButtonStyle.link, url=self.msglink)
+        link_button = discord.ui.Button(label=f"Message Link ({src_lang})", style=discord.ButtonStyle.link, url=msglink)
         self.add_item(link_button)
 
 
@@ -23,9 +22,15 @@ class Translate(commands.Cog):
     @commands.message_command(name="Translate")
     async def translate(self, ctx, message: discord.Message):
         content = message.content
-        translated = translator.translate(content, dest='en').text
 
-        return await ctx.send_response(translated, view=TranslateView(message.jump_url))
+        translated = translator.translate(content, dest='en')
+        translated_text = translated.text
+        src_lang = LANGUAGES[translated.src].capitalize()
+
+        return await ctx.send_response(
+            translated_text,
+            view=TranslateView(message.jump_url, src_lang),
+        )
 
 
 
