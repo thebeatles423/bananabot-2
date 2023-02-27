710ccb16b0cecf81a9f115606eaed7ceeb640523
ksbidlack <ksbidlack@gmail.com>
2023-02-26 19:32:59 -0800

    add autocomplete for vc command

--- src/ext/misc.py
+++ src/ext/misc.py
@@ -9,21 +9,20 @@ class Misc(commands.Cog):
     @commands.slash_command(name="vc", description="Send a voice chat link without having to use developer mode")
     async def vc(
         self, ctx, 
-        vc_name: discord.Option(str, "The name of the vc", required=False, default=None)):
-        if vc_name:
-            try:
-                vc_channel = discord.utils.get(ctx.guild.channels, name=vc_name)
-            except:
-                return await ctx.send_response("Invalid voice channel!")
-            if str(vc_channel.type) == "voice":
-                return await ctx.send_response(vc_channel.mention)
-            else:
-                return await ctx.send_response(f"{vc_channel.mention} is not a voice channel!", ephemeral=True)
-        elif ctx.author.voice: # the author is in a vc
-            vc_channel = discord.utils.get(ctx.guild.channels, name=vc_name)
-            return await ctx.send_response(vc_channel.mention)    
+        vc: discord.Option(
+            discord.VoiceChannel,
+            required=False
+        )
+    ):
+        if vc:
+            return await ctx.send_response(vc.mention)
+        elif not vc and (voice := ctx.author.voice):
+            return await ctx.send_response(voice.channel.mention)
         else:
-            return await ctx.send_response("Please specify a voice channel!")
+            return await ctx.send_response(
+                "You're not in a voice channel!",
+                ephemeral=True
+            )
 
 
 def setup(bot):
